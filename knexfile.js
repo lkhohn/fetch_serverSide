a9b2ed9a77bdd7d80fa3825315a894fc76032158
LKHohn <lkhohn@gmail.com>
2016-03-16 07:21:14 -0600

    heroku deploy

--- knexfile.js
+++ knexfile.js
@@ -43,13 +43,32 @@
 //
 // };
 
+// module.exports = {
+//   development: {
+//     client:'pg',
+//     connection: 'postgres://localhost:5432/fetchapp'
+//   },
+//   production: {
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL
+//   }
+// };
+
+
+
+// Update with your config settings.
+require('dotenv').load();
+
 module.exports = {
+
   development: {
-    client:'pg',
-    connection: 'postgres://localhost:5432/fetchapp'
+    client: 'pg',
+    connection: process.env.DATABASE_HEROKU + '?ssl=true'
   },
+
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_HEROKU + '?ssl=true'
   }
+
 };
